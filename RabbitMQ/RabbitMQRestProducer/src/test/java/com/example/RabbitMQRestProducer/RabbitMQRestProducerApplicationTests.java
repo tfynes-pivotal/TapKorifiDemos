42fe8096e68b01fa00d1f774b3ae07992e5a2de3
Mark Fynes <tfynes@vmware.com>
2024-04-17 17:23:11 -0400

    .

---
+++ RabbitMQ/RabbitMQRestProducer/src/test/java/com/example/RabbitMQRestProducer/RabbitMQRestProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RabbitMQRestProducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMQRestProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
